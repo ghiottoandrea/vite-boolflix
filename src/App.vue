6b4323a680d4a322d80e0fbaced0be6d503837cf
ghiottoandrea <andreaghiotto9@gmail.com>
2024-04-03 15:57:57 +0200

    Add milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,46 @@
-<script >
+<script>
+import { state } from './state.js';
+
+import searchBox from './components/searchBox.vue';
+import axios from 'axios';
 
 export default {
-  name: `App`,
+    name: `App`,
 
-  components: {
-
-  }
+    components: {
+        searchBox
+    },
+    data() {
+        return {
+            state,
+            movies: []
+        }
+    },
+    methods: {
+        searchMovies(searchMovie) {
+            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=bc46d729b2821cc15efdf5ada3a491ac&query=${searchMovie}`)
+                .then(response => {
+                    console.log('searching...', searchMovie)
+                    this.movies = response.data.results
+                    console.log(this.movies);
+                })
+        }
+    }
 }
 </script>
 
 <template>
 
+    <searchBox @performSearch="searchMovies" />
+
+<ul v-for="movie in movies">
+    <li>Titolo: {{ movie.title }}</li>
+    <li>Titolo Originale: {{ movie.original_title }}</li>
+    <li>Lingua: {{ movie.original_language }}</li>
+    <li>Voto: {{ movie.vote_average }}</li>
+
+</ul>
+
 </template>
 
-<style >
-
-</style>
- 
+<style></style>
---
+++ src/components/searchBox.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'searchBox',
+    data(){
+        return{
+            searchMovie: ''
+        }
+    },
+    methods: {
+        searchMovies(){
+            this.$emit('performSearch', this.searchMovie)
+        }
+    }
+}
+
+</script>
+
+<template>
+
+<div class="searchBox">
+    <input type="text" v-model="searchMovie" placeholder="Search a movie">
+    <button @click="searchMovies" >Search</button>
+</div>
+
+</template>
+
+<style>
+
+</style>
